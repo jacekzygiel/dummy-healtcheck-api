c6a44d0b2c08c0a09335b344a1b00a2fc66e6215
Jacek Zygiel <jacekzygiel@gmail.com>
2020-03-13 11:21:42 +0100

    Inital commit

---
+++ src/main/java/com/zygiel/HealthCheckApi/HealthCheckApiApplication.java
@@ -0,0 +1,13 @@
+package com.zygiel.HealthCheckApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCheckApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCheckApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zygiel/HealthCheckApi/HealthCheckApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zygiel.HealthCheckApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCheckApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
